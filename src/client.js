d87288e81da7836cf9adc723d57a2a3d9bcdde8f
Endre Vegh <endre.vegh@yahoo.com>
2018-07-15 09:47:02 +0100

    client code

--- src/client.js
+++ src/client.js
@@ -1,18 +1,39 @@
-// import startDemo from './Demo';
-
-// startDemo();
-
 import React from './Fiber';
 
-import { render } from './Fiber/Engine';
+import { render, subscribeToRenderCommit } from './Fiber/Engine';
 
 const root = document.getElementById('root');
 
+subscribeToRenderCommit(renderInfo => console.log('Rendering is complete!'));
+
 const element = (
-  <div style={{ backGroundColor: 'red' }}>
+  <div style={{ backgroundColor: 'red' }}>
     <div first={true}>hey</div>
     <div second={true}>hi</div>
   </div>
 );
 
 render(element, root);
+
+const otherElement = (
+  <div style={{ backgroundColor: 'red' }}>
+    <button first={true}>hey</button>
+    <button second={true}>hi</button>
+    <button>third</button>
+  </div>
+);
+
+setTimeout(() => {
+  render(otherElement, root);
+}, 2000);
+
+const otherElement2 = (
+  <div style={{ backgroundColor: 'red' }}>
+    <div second={true}>hi</div>
+    <div>third</div>
+  </div>
+);
+
+setTimeout(() => {
+  render(otherElement2, root);
+}, 4000);
